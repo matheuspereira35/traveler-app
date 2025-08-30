9b955966e8566dc90e0eb58039b32cfd748aa825
Matheus Pereira <matheus_-pereira@hotmail.com>
2025-08-29 21:50:03 -0300

    selects puxando infos da api

--- src/app/lugares/lugar/lugar.component.ts
+++ src/app/lugares/lugar/lugar.component.ts
@@ -1,19 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Categoria } from '../../categorias/categoria';
+import { CategoriaService } from '../../categorias/categoria.service';
+import { LugarService } from '../lugar.service';
 
 @Component({
   selector: 'app-lugar',
   standalone: false,
   templateUrl: './lugar.component.html',
-  styleUrl: './lugar.component.scss'
+  styleUrl: './lugar.component.scss',
 })
-export class LugarComponent {
-
+export class LugarComponent implements OnInit {
   camposForm: FormGroup;
   categorias: Categoria[] = [];
 
-  constructor(){
+  constructor(
+    private categoriaService: CategoriaService,
+    private service: LugarService
+  ) {
     this.camposForm = new FormGroup({
       nome: new FormControl('', Validators.required),
       categoria: new FormControl('', Validators.required),
@@ -23,7 +27,30 @@ export class LugarComponent {
     });
   }
 
-  salvar(){
-    console.log('Valores: ', this.camposForm.value);
+  ngOnInit(): void {
+    this.categoriaService.obterTodas().subscribe({
+      next: (listaCategorias) => (this.categorias = listaCategorias),
+    });
+  }
+
+  salvar() {
+
+    this.camposForm.markAllAsTouched();
+  
+    if(this.camposForm.valid){
+      this.service.salvar(this.camposForm.value).subscribe({
+        next: (lugar) => {
+          console.log('Cadastrado com sucesso!', lugar);
+          this.camposForm.reset();
+        },
+        error: (erro) => console.error('Erro ao cadastrar!', erro),
+      });
+    }
+
+  }
+
+  isCampoInvalido(nomeCampo: string): boolean {
+    const campo = this.camposForm.get(nomeCampo);
+    return campo?.invalid && campo.touched && campo.errors?.['required'];
   }
 }
